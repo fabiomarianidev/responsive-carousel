5b29a5c63d4b14e9d376bf8c2495e8b0581b6f34
fabiomarianidev <70094393+fabiomarianidev@users.noreply.github.com>
2020-10-08 22:54:36 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,158 @@
+const setCarouselSize = () => {
+    const windowWidth = document.documentElement.clientWidth;
+    let carouselWidth = 1200;
+    let numberOfImages = 3;
+
+    if (windowWidth < 576) {
+        carouselWidth = windowWidth * 0.9;
+        numberOfImages = 1;
+    } else if (windowWidth < 768) {
+        carouselWidth = windowWidth * 0.9;
+        numberOfImages = 2;
+    } else if (windowWidth < 992) {
+        carouselWidth = windowWidth * 0.9;
+        numberOfImages = 3;
+    } else if (windowWidth < 1200) {
+        carouselWidth = windowWidth * 0.9;
+        numberOfImages = 4;
+    } else {
+        carouselWidth = 1200;
+        numberOfImages = 5;
+    }
+
+    return [
+        carouselWidth,
+        numberOfImages
+    ]
+
+
+}
+
+const [carouselWidth, numberOfImages] = setCarouselSize();
+// const carouselWidth = 1200;
+// const numberOfImages = 3;
+
+document.documentElement.style.setProperty('--carousel-width', `${carouselWidth}px`);
+
+const itemWidth = carouselWidth / numberOfImages;
+document.documentElement.style.setProperty('--item-width', `${itemWidth}px`);
+
+const carouselHeight = itemWidth * 2/3;
+document.documentElement.style.setProperty('--carousel-height', `${carouselHeight}px`)
+
+const images = [
+    "https://picsum.photos/600/400?random=1",
+    "https://picsum.photos/600/400?random=2",
+    "https://picsum.photos/600/400?random=3",
+    "https://picsum.photos/600/400?random=4",
+    "https://picsum.photos/600/400?random=5",
+    "https://picsum.photos/600/400?random=6",
+    "https://picsum.photos/600/400?random=7",
+    "https://picsum.photos/600/400?random=8",
+    "https://picsum.photos/600/400?random=9",    
+]
+
+const rightLimit = (images.length - numberOfImages) * itemWidth; 
+
+const checkLimits = () => { //hides left-right buttons if the first or last image is reached
+    if (position >= 0) {
+        document.querySelector('.previous').classList.add('hidden');
+    } else {
+        document.querySelector('.previous').classList.remove('hidden');
+    }
+    if (position <= -rightLimit) {
+        document.querySelector('.next').classList.add('hidden');
+    } else {
+        document.querySelector('.next').classList.remove('hidden');
+    }
+}
+
+// previous/next buttons 
+const handlePrev = () => {
+    if(position < 0) {
+        currentPosition = position + itemWidth;
+        document.documentElement.style.setProperty('--position', `${currentPosition}px`);
+        position = currentPosition;
+        checkLimits();
+    }
+}
+
+const handleNext = () => {
+    if (position > -rightLimit) {
+        currentPosition = position - itemWidth;
+        document.documentElement.style.setProperty('--position', `${currentPosition}px`);
+        position = currentPosition;
+        checkLimits();
+    }
+}
+
+// drag handlers
+let isDragging = false;
+let dragStart = null;
+let position = 0;
+let currentPosition = null; 
+
+const handleDragStart = (evt) => {
+    if (evt.touches) {evt = evt.touches[0]}; //if it is a touch event, use the first touch pointer 
+    isDragging = true;
+    dragStart = evt.clientX;
+}
+
+const handleDrag = (evt) => {
+    if (isDragging) {
+        if (evt.touches) { //if it is a touch event, use the first touch pointer
+            evt.preventDefault(); //prevent browser back associated with swipe left
+            evt = evt.touches[0];
+        }; 
+        let dragLength = evt.clientX - dragStart;
+        currentPosition = position + dragLength;
+        if (currentPosition > 0 ) {currentPosition = 0};
+        if (currentPosition < -rightLimit) {currentPosition = -rightLimit};
+        document.documentElement.style.setProperty('--position', `${currentPosition}px`)
+    }
+}
+
+const handleDragEnd = (evt) => {
+    // snaps in order to have the image border on the left edge
+    const modulo = -currentPosition % itemWidth;
+    if (modulo < itemWidth /2 ) {
+        currentPosition = currentPosition + modulo;
+    } else {
+        currentPosition = currentPosition + modulo - itemWidth;
+    }
+    document.documentElement.style.setProperty('--position', `${currentPosition}px`);
+    position = currentPosition;
+    checkLimits();
+    isDragging = false;
+}
+
+
+
+
+// load images into carousel
+images.forEach ((item, index) => {
+    const div = document.createElement('div');
+    div.classList.add('item')
+    const img = document.createElement('img');
+    img.src=item;
+    div.appendChild(img);
+    document.querySelector(".carousel-filmstrip").appendChild(div);
+})
+
+// check if previous/next buttons need to be displayed
+checkLimits();
+
+//add event listeners for dragging
+const carouselContainer = document.querySelector(".carousel-container");
+//mouse events
+carouselContainer.addEventListener('mousedown', handleDragStart, false);
+carouselContainer.addEventListener('mousemove', handleDrag, false);
+carouselContainer.addEventListener('mouseup', handleDragEnd, false);
+//touch events
+carouselContainer.addEventListener('touchstart', handleDragStart , false);
+carouselContainer.addEventListener('touchmove', handleDrag, false);
+carouselContainer.addEventListener('touchend', handleDragEnd, false);
+
+//add event listener for previous/next buttons
+document.querySelector('.previous').addEventListener('click', handlePrev, false);
+document.querySelector('.next').addEventListener('click', handleNext, false);
